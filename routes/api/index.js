addec2374404b01e42cfa2bd7db99460ddc308a4
anagha778 <anagha778@gmail.com>
2021-04-08 23:11:44 -0400

    fixed routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,8 +3,8 @@ const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
 // add prefix of `/users` to routes created in `user-routes.js`
-router.use('/users', userRoutes);
+router.use('/users', userRuoutes);
 // add prefix of `/thoughts` to routes created in `thought-routes.js`
-router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtsRoutes);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,13 +1,5 @@
 const router = require('express').Router();
-const {
-    getAllUser,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-    addFriend,
-    deleteFriend
-  } = require('../../controllers/user-controller');
+
 
 
 // Set up GET all and POST at /api/users
@@ -23,9 +15,5 @@ router
   .put(updateUser)
   .delete(deleteUser);
 
-router
-  .route('/:userId/friends/:friendId')
-  .post(addFriend)
-  .delete(deleteFriend);
 
 module.exports = router;
